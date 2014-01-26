c6fb11d480eb77b9e0877d0d697aed1a7af9ed1b
Robert Young <rob.young@canarie.ca>
2014-01-26 17:17:56 -0500

    Cleaned up locks.

--- server.py
+++ server.py
@@ -20,61 +20,57 @@ class SensorStore:
         self.lock.release()
         
     def get_conn(self):
-        return sqlite3.connect('test.db', check_same_thread=False, timeout=20)
+        return sqlite3.connect('test.db', check_same_thread=False)
         
     # return id
     def create_sensor(self, name, units):
-        self.__acquire_lock()
-        conn = self.get_conn()
-        c = conn.cursor()
-        c.execute("insert into sensor(name, unit) values ( ?, ? )", ( name, units ) )
-        conn.commit()
-        conn.close()
-        self.__release_lock()
+        with self.lock:
+            conn = self.get_conn()
+            c = conn.cursor()
+            c.execute("insert into sensor(name, unit) values ( ?, ? )", ( name, units ) )
+            conn.commit()
+            conn.close()
         
     def get_sensor(self, name):
-        self.__acquire_lock()
-        conn = self.get_conn()
-        c = conn.cursor()
-        c.execute("select id from sensor where name = ?", (name,))
-        result = c.fetchone()
-        conn.close()
-        self.__release_lock()
-        
-        if result is None:
-            return None
-        else:
-            return result[0]
+        with self.lock:
+            conn = self.get_conn()
+            c = conn.cursor()
+            c.execute("select id from sensor where name = ?", (name,))
+            result = c.fetchone()
+            conn.close()
+            
+            if result is None:
+                return None
+            else:
+                return result[0]
         
     def put_float_data(self, sensor, value, time):
-        self.__acquire_lock()
-        conn = self.get_conn()
-        c = conn.cursor()
-        c.execute("insert into sensor_float_data(val, timestamp, sensor_id) values( ?, ?, ? )", (value, calendar.timegm(time.utctimetuple()), sensor))
-        conn.commit()
-        self.__release_lock()
+        with self.lock:
+            conn = self.get_conn()
+            c = conn.cursor()
+            c.execute("insert into sensor_float_data(val, timestamp, sensor_id) values( ?, ?, ? )", (value, calendar.timegm(time.utctimetuple()), sensor))
+            conn.commit()
         
     def get_latest_float_reading(self, sensor):
-        self.__acquire_lock()
-        conn = self.get_conn()
-        c = conn.cursor()
-        c.execute("select max(timestamp), val from sensor_float_data where sensor_id = ?", (sensor, ))
-        result = c.fetchone()
-        if result is not None and type(result) is tuple and result[0] is None:
-            result = None
-        conn.close()
-        self.__release_lock()
-        return result
+        with self.lock:
+            conn = self.get_conn()
+            c = conn.cursor()
+            c.execute("select max(timestamp), val from sensor_float_data where sensor_id = ?", (sensor, ))
+            result = c.fetchone()
+            if result is not None and type(result) is tuple and result[0] is None:
+                result = None
+            conn.close()
+
+            return result
         
     def init_sensor(self, name, units):
-        self.__acquire_lock()
-        sensor = self.get_sensor(name)
-        
-        if sensor is None:
-            sensor = self.create_sensor(name, units)
-        self.__release_lock()
-        
-        return sensor
+        with self.lock:
+            sensor = self.get_sensor(name)
+            
+            if sensor is None:
+                sensor = self.create_sensor(name, units)
+            
+            return sensor
     
     
 temperature = "temp1"
@@ -112,27 +108,12 @@ class DHTSensorSource(threading.Thread):
             self.sensor_store.put_float_data(self.temperature, float(temp), timestamp )
             self.sensor_store.put_float_data(self.humidity, float(humidity), timestamp )
             
-            time.sleep(40)
+            for i in range(0, 40):
+                if not self.shutdown:
+                    time.sleep(1)
+            
         print "Shutting down DHT writer for pin {}".format(self.pin)
         return None
-  
-class SlowWriter(threading.Thread):
-    '''Testing class simulating a thread writing sensor data'''
-    def __init__(self, sensor, sleep_delay, sensor_store):
-        threading.Thread.__init__(self)
-        self.sensor = sensor
-        self.sleep_delay = sleep_delay
-        self.sensor_store = sensor_store
-        self.shutdown = False
-        
-    def run ( self ):
-        for i in range(0, 2000):
-            self.sensor_store.put_float_data(self.sensor, i * 1.1, datetime.datetime.utcnow())
-            time.sleep(self.sleep_delay)
-            
-            if self.shutdown:
-                print "Shutting down slow writer thread!"
-                return None
           
 class UpdateReader(threading.Thread):
     
@@ -175,18 +156,14 @@ try:
   thread1=DHTSensorSource(therm, hum, "4", store)
   thread1.start()
   thread2=UpdateReader(therm, "Temperature", 1, store)
-  #thread3=UpdateReader(hum, 2, store)
   thread2.start()
-  #thread3.start()
   while True:
       time.sleep(1)
 except (KeyboardInterrupt, SystemExit):
   print '\n! Received keyboard interrupt, quitting threads.\n'
   thread1.shutdown = True
   thread2.shutdown = True
-  #thread3.shutdown = True
   thread1.join()
   thread2.join()
-  #thread3.join()
   print "Exit main"
 
